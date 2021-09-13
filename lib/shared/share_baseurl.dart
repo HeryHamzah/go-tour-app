98fc6b7a208a8cc7eaa62ce61e34bcad10d03c84
HeryHamzah <hamzahhery23@gmail.com>
2021-09-13 23:47:10 +0800

    Update

--- lib/shared/share_baseurl.dart
+++ lib/shared/share_baseurl.dart
@@ -3,10 +3,10 @@ part of 'shared.dart';
 String dbUrl = "http://10.0.2.2/gotour/";
 
 class BaseUrl {
-  static String regisUser = dbUrl + "signup.php";
-  static String getUser = dbUrl + "getuser.php";
-  static String getLocation = dbUrl + "getlocation.php";
+  static String regisUser = dbUrl + "user_signup.php";
+  static String getUser = dbUrl + "get_user.php";
+  static String getLocation = dbUrl + "get_locations.php";
   static String getAssets = dbUrl + "images/assets/";
   static String getUserImages = dbUrl + "images/user_upload/";
-  static String uploadUserImage = dbUrl + "uploaduserimage.php";
+  static String uploadUserImage = dbUrl + "upload_user_image.php";
 }
