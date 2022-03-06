52cada77ea3a63924e8fc6853dd618a6bdbb960a
HeryHamzah <hamzahhery23@gmail.com>
2022-03-06 23:27:32 +0800

    Update

--- lib/ui/pages/user/map_screen.dart
+++ lib/ui/pages/user/map_screen.dart
@@ -5,10 +5,22 @@ class MapScreen extends StatefulWidget {
   State<MapScreen> createState() => _MapScreenState();
 }
 
+BitmapDescriptor destinationMarker;
+
 class _MapScreenState extends State<MapScreen> {
-  Completer<GoogleMapController> _controller = Completer();
-  Position _currentPosition;
-  String _currentAddress;
+  // Completer<GoogleMapController> _controller = Completer();
+  Position _currentPosition =
+      Position(latitude: -5.140362, longitude: 119.4830809);
+  Marker _origin;
+  Set<Marker> _destinations = {dest1, dest2, dest3};
+  // String _currentAddress;
+
+  void setCustomMarker() async {
+    destinationMarker = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(), 'assets/destination_marker.png');
+  }
+
+  GoogleMapController _googleMapController;
 
   Future<void> getCurrentLocation() async {
     await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
@@ -21,15 +33,21 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
-  // static final CameraPosition _initialCameraPosition = CameraPosition(
-  //   target: LatLng(37.42796133580664, -122.085749655962),
-  //   zoom: 14.4746,
-  // );
+  static final CameraPosition _initialCameraPosition = CameraPosition(
+    target: LatLng(-5.140362, 119.4830809),
+    zoom: 14,
+  );
 
   @override
   void initState() {
-    super.initState();
     getCurrentLocation();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _googleMapController.dispose();
+    super.dispose();
   }
 
   @override
@@ -54,7 +72,42 @@ class _MapScreenState extends State<MapScreen> {
             target:
                 LatLng(_currentPosition.latitude, _currentPosition.longitude),
             zoom: 14,
-          )),
+          ),
+          markers: _destinations,
+          onMapCreated: (controller) => _googleMapController = controller),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.center_focus_strong,
+            color: backColor,
+          ),
+          onPressed: () {
+            _googleMapController.animateCamera(
+                CameraUpdate.newCameraPosition(_initialCameraPosition));
+          }),
     );
   }
 }
+
+Marker dest1 = Marker(
+    markerId: MarkerId('3'),
+    icon: destinationMarker,
+    position: LatLng(-5.1403567000, 119.4808922000),
+    draggable: true,
+    onTap: () {
+      print("Tapped");
+    },
+    infoWindow: InfoWindow(title: "Tes", snippet: "Tes"));
+
+Marker dest2 = Marker(
+    icon: destinationMarker,
+    markerId: MarkerId('1'),
+    position: LatLng(-5.1545372000, 119.4210330000),
+    infoWindow: InfoWindow(title: "Tes2", snippet: "Tes2"));
+
+Marker dest3 = Marker(
+    icon: destinationMarker,
+    markerId: MarkerId('2'),
+    position: LatLng(-5.1384045000, 119.4824149000),
+    infoWindow: InfoWindow(title: "Tes4", snippet: "Tes4"));
